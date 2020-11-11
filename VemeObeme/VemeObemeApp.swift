3796357e95367c23d65ac50d233196e480592935
David Bárcenas Durán <davisbd100@gmail.com>
2020-11-11 09:53:57 -0600

    Initial Commit

---
+++ VemeObeme/VemeObemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VemeObemeApp.swift
+//  VemeObeme
+//
+//  Created by David Bárcenas Duran on 11/11/20.
+//
+
+import SwiftUI
+
+@main
+struct VemeObemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
