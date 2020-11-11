32e36c7002a32dc3fd329eb6a0cf47ab864ce15f
David Bárcenas Durán <davisbd100@gmail.com>
2020-11-11 10:20:43 -0600

    Textfields in login

--- VemeObeme/ContentView.swift
+++ VemeObeme/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Image("")
+                .frame(width: 216, height: 98)
+            Text("Iniciar Sesión")
+                .foregroundColor(.green)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 168, maxWidth: 200, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 41, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .font(.custom("Avenir Heavy", size: 30))
+            SecureField("Contraseña", text:$password)
+                .foregroundColor(.green)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .font(.custom("Avenir Book", size: 15))
+            TextField("Contraseña", text:$password)
+                .foregroundColor(.green)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .font(.custom("Avenir Book", size: 15))
+            Spacer()
+        }
     }
 }
 
