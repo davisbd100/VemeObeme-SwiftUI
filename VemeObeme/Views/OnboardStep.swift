329054dca8568369f0c4d936b99af1ad0dee707a
David Bárcenas Durán <davisbd100@gmail.com>
2020-11-23 22:39:48 -0600

    Adding of lottie

--- VemeObeme/Views/OnboardStep.swift
+++ VemeObeme/Views/OnboardStep.swift
@@ -6,13 +6,16 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct OnboardStep: View {
     @State var imageLocation: String = "LogoOBEME"
     @State var titleStep: String = "Registra debilidades del proceso educativo en la unidad médica"
     @State var descriptionStep: String = "El cumplimiento de los programas académico y operativo, la supervisión de actividades clínicas, las evaluaciones, el trato que recibes, etc."
+    let animation = AnimationView()
     var body: some View {
         VStack (alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, content: {
+            
             Image(imageLocation)
                 .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 212, maxWidth: 250, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 212, maxHeight: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .padding()
--- VemeObeme/Views/OnboardView.swift
+++ VemeObeme/Views/OnboardView.swift
@@ -13,15 +13,15 @@ struct OnboardView: View {
         VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content: {
             TabView(selection: $currentTab,
                     content:  {
-                        OnboardStep(imageLocation: "animation_500_kbtxjxmo", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
+                        OnboardStep(imageLocation: "health-report", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
                             .tag(1)
-                        OnboardStep(imageLocation: "animation_500_kbtxjxmo", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
+                        OnboardStep(imageLocation: "health-report", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
                             .tag(2)
-                        OnboardStep(imageLocation: "animation_500_kbtxjxmo", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
+                        OnboardStep(imageLocation: "health-report", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
                             .tag(3)
-                        OnboardStep(imageLocation: "animation_500_kbtxjxmo", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
+                        OnboardStep(imageLocation: "health-report", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
                             .tag(4)
-                        OnboardStep(imageLocation: "animation_500_kbtxjxmo", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
+                        OnboardStep(imageLocation: "health-report", titleStep: "¡Hola!", descriptionStep: "Si eres interno, pasante o residente de medicina esta aplicación es para ti.")
                             .tag(5)
                     })
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
