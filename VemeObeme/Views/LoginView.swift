de31666c753764cfa76a6eaa3873ecb06defd528
David Bárcenas Durán <davisbd100@gmail.com>
2020-11-23 22:31:56 -0600

    Adding of lottie dependency

--- VemeObeme/Views/LoginView.swift
+++ VemeObeme/Views/LoginView.swift
@@ -50,11 +50,9 @@ struct LoginView: View {
                 )
             
             Button(action: {
-                self.disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
                 if (checkFields()){
                     viewModel.tryLogin(username: username, password: password)
                 }
-                self.disabled(false)
             }, label: {
                 Text("Iniciar Sesion")
                     .foregroundColor(.white)
