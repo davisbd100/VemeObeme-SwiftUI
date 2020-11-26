748d60efd736085ed690182b28d440080fbdc2b2
David Bárcenas Durán <davisbd100@gmail.com>
2020-11-25 23:55:03 -0600

    Loading image

--- VemeObeme/Views/LoginView.swift
+++ VemeObeme/Views/LoginView.swift
@@ -9,75 +9,77 @@ import SwiftUI
 
 struct LoginView: View {
     @StateObject var viewModel = LoginViewModel()
-    @State private var isEmailValid: Bool = false
     @State private var isLogin: Bool = false
     @State private var errorMessages: String = "Login"
     
     
     var body: some View {
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
-
-            Image("LogoOBEME")
-                .frame(width: 216, height: 98)
-            
-            Text("Iniciar Sesión")
-                .foregroundColor(.green)
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 168, maxWidth: 200, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 41, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .font(.custom("Avenir Heavy", size: 30))
-            
-            ValidatorField(value: $viewModel.username, placeholder: "Correo", keyType: .emailAddress, borderlineColor: viewModel.isMailValid || viewModel.username.isEmpty ? Color.black : Color.red)
-
-            ValidatorSecureField(value: $viewModel.password, placeholder: "Contraseña")
-            Button(action: {
-                let dispatch = DispatchGroup()
-                isLogin.toggle()
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .center), content: {
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10, content: {
+                Image("LogoOBEME")
+                    .frame(width: 216, height: 98)
                 
-                dispatch.enter()
-                viewModel.tryLogin(){errorcode in
-                    dispatch.leave()
-                }
-                dispatch.notify(queue: .main){
+                Text("Iniciar Sesión")
+                    .foregroundColor(.green)
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 168, maxWidth: 200, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 41, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .font(.custom("Avenir Heavy", size: 30))
+                
+                ValidatorField(value: $viewModel.username, placeholder: "Correo", keyType: .emailAddress, borderlineColor: viewModel.isMailValid || viewModel.username.isEmpty ? Color.black : Color.red)
+                
+                ValidatorSecureField(value: $viewModel.password, placeholder: "Contraseña")
+                Button(action: {
+                    let dispatch = DispatchGroup()
                     isLogin.toggle()
-                }
-            }, label: {
-                Text("Iniciar Sesion")
-                    .foregroundColor(.white)
-                    .font(.custom("Avenir Heavy", size: 15))
-                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    dispatch.enter()
+                    viewModel.tryLogin(){errorcode in
+                        dispatch.leave()
+                    }
+                    dispatch.notify(queue: .main){
+                        isLogin.toggle()
+                    }
+                }, label: {
+                    Text("Iniciar Sesion")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Heavy", size: 15))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                })
+                .background(Color.blue)
+                .cornerRadius(10.0)
+                
+                
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Registrate")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Heavy", size: 15))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                })
+                .background(Color.green)
+                .cornerRadius(10.0)
+                
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("¿Que es el Obeme?")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Heavy", size: 15))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                })
+                .background(Color.yellow)
+                .cornerRadius(10.0)
+                
             })
-            .background(Color.blue)
-            .cornerRadius(10.0)
-            .alert(isPresented: $isLogin, content: {
-                Alert(title: Text(errorMessages), message: Text(errorMessages))
-            })
-            .alert(isPresented: $isEmailValid, content: {
-                Alert(title: Text("Email invalido, revisa que sea correcto y reintenta"))
-            })
-            
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("Registrate")
-                    .foregroundColor(.white)
-                    .font(.custom("Avenir Heavy", size: 15))
-                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            })
-            .background(Color.green)
-            .cornerRadius(10.0)
-            
-            Spacer()
-            
-            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                Text("¿Que es el Obeme?")
-                    .foregroundColor(.white)
-                    .font(.custom("Avenir Heavy", size: 15))
-                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: 53, maxHeight: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-            })
-            .background(Color.yellow)
-            .cornerRadius(10.0)
-            
+            .padding(.trailing)
+            .padding(.leading)
+            .padding(.bottom)
+            if isLogin{
+                GeometryReader { geo in
+                    LoadingView()
+                        .position(x:geo.frame(in:.global).midX,y:geo.frame(in:.global).midY)
+                }.background(Color.black.opacity(0.45).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/))
+            }
         })
-        .padding(.trailing)
-        .padding(.leading)
-        .padding(.bottom)
     }
 }
 
@@ -169,9 +171,25 @@ struct Validator: View {
     }
 }
 
+struct LoadingView: View {
+    
+    var body: some View {
+        VStack{
+            Text("Iniciando Sesión")
+                .foregroundColor(.green)
+                .font(.custom("Avenir Heavy", size: 30))
+            LottieView(filename: "electro", loopMode: .loop)
+                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: 200, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: 200, alignment: Alignment(horizontal: .center, vertical: .center))
+        }
+        .background(Color.white.opacity(0.60))
+        .padding()
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
             .previewDevice("iPhone 11")
     }
 }
+
