e4922ec03446740f1012a58b9648c6da0358b6b5
David Bárcenas Durán <davisbd100@gmail.com>
2021-02-04 17:59:08 -0600

    addBorder separation

---
+++ VemeObeme/Utils/AddBorder.swift
@@ -0,0 +1,17 @@
+//
+//  addBorder.swift
+//  VemeObeme
+//
+//  Created by David Bárcenas Duran on 04/02/21.
+//
+
+import SwiftUI
+
+extension View {
+    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
+        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
+        return clipShape(roundedRect)
+             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
+    }
+}
+
--- VemeObeme/Views/RegisterView.swift
+++ VemeObeme/Views/RegisterView.swift
@@ -195,15 +195,6 @@ struct FourthRegisterView: View {
 }
 
 
-
-
-extension View {
-    public func addBorder<S>(_ content: S, width: CGFloat = 1, cornerRadius: CGFloat) -> some View where S : ShapeStyle {
-        let roundedRect = RoundedRectangle(cornerRadius: cornerRadius)
-        return clipShape(roundedRect)
-             .overlay(roundedRect.strokeBorder(content, lineWidth: width))
-    }
-}
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
