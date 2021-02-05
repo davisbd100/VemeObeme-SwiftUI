f649289df5eb4c0653c601165e4e52c1964f5af8
David Bárcenas Durán <davisbd100@gmail.com>
2021-02-04 20:30:06 -0600

    Fixing the transgression observation view

--- VemeObeme/Views/RootView.swift
+++ VemeObeme/Views/RootView.swift
@@ -11,7 +11,8 @@ struct RootView: View {
     @State var login = false
     @State var isLoggedIn = (UserDefaults.standard.bool(forKey: "isLoggedIn"))
     var body: some View {
-            UserDefaults.standard.setCodableObject(User(correo: "davisbd100@gmail.com", jwt: "token", nombres: "David", apellidos: "Bárcenas Durán", email: "davisbd100@gmail.com", fechaNacimiento: "24&05/99", genero: "Masculino", celular: "2288455625", university: Univesity(pais: "México", nombreUniversidad: "Universidad Veracruzana"), healtUnit: HealthUnit(healthUnitName: "Clínica 11 IMSS")), forKey: "currentUser")
+        UserDefaults.standard.setValue(true, forKey: "isLoggedIn")
+        UserDefaults.standard.setCodableObject(User(correo: "davisbd100@gmail.com", jwt: "token", nombres: "David", apellidos: "Bárcenas Durán", email: "davisbd100@gmail.com", fechaNacimiento: "24&05/99", genero: "Masculino", celular: "2288455625", university: Univesity(pais: "México", nombreUniversidad: "Universidad Veracruzana"), healtUnit: HealthUnit(healthUnitName: "Clínica 11 IMSS")), forKey: "currentUser")
         return Group {
             NavigationView{
                 if (isLoggedIn){
--- VemeObeme/Views/TransgressionObservationView.swift
+++ VemeObeme/Views/TransgressionObservationView.swift
@@ -1,63 +1,71 @@
 //
-//  TransgressionObservation.swift
+//  TransgresionObservationView.swift
 //  VemeObeme
 //
-//  Created by David Bárcenas Duran on 22/01/21.
+//  Created by David Bárcenas Duran
 //
 
 import SwiftUI
 
-struct TransgressionObservation: View {
+
+struct TransgressionObservationView: View {
     var body: some View {
         VStack{
             Text("¿Qué tipo de falta a derechos sufriste?")
-                .font(.custom("Avenir Mediym", size: 18))
-            padding()
-            Group{
-                HStack{
+                .font(.custom("Avenir Mediym", size: 18)).padding()
+                .multilineTextAlignment(.center)
+            ScrollView{
+                LazyVGrid(columns: [GridItem(.flexible()),
+                            GridItem(.flexible())], content: {
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         VStack{
                             Image("ic_castigos")
                             Text("Castigos")
+                                .font(.custom("Avenir Book", size: 18))
+                                .foregroundColor(.black)
                         }
                     })
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         VStack{
                             Image("ic_acoso")
-                            Text("Acoso Sexual")
+                            Text("Acoso sexual")
+                                .font(.custom("Avenir Book", size: 18))
+                                .foregroundColor(.black)
                         }
                     })
-                }
-                HStack{
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         VStack{
                             Image("ic_violencia_verbal")
                             Text("Violencia verbal")
+                                .font(.custom("Avenir Book", size: 18))
+                                .foregroundColor(.black)
                         }
                     })
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         VStack{
                             Image("ic_violencia_fisica")
                             Text("Violencia física")
+                                .font(.custom("Avenir Book", size: 18))
+                                .foregroundColor(.black)
                         }
                     })
-                }
-                HStack{
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         VStack{
                             Image("ic_otro")
-                            Text("Otros")
+                            Text("Otro")
+                                .font(.custom("Avenir Book", size: 18))
+                                .foregroundColor(.black)
                         }
                     })
-                }
+                })
+                .padding(.horizontal)
             }
         }
-        
     }
 }
 
-struct TransgressionObservation_Previews: PreviewProvider {
+struct TransgressionObservationView_Previews: PreviewProvider {
     static var previews: some View {
-        TransgressionObservation()
+        TransgressionObservationView()
     }
 }
