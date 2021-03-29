4754c5bef050908246da1e63ce2db3afecd16a60
David Bárcenas Durán <davisbd100@gmail.com>
2021-03-29 15:38:50 -0600

    Delete of unecesary printing

--- VemeObeme/Network/UserManagementPetitionManager.swift
+++ VemeObeme/Network/UserManagementPetitionManager.swift
@@ -69,8 +69,6 @@ class UserManagementPetitionManager {
                 return
             }
             if (responseData.statusCode == 200){
-                let json = try? JSONSerialization.jsonObject(with: sentData, options: [])
-                debugPrint(json)
                 completion(Response(data: sentData), nil)
             }else{
                 completion(nil, NSError(domain: "failed login", code: responseData.statusCode, userInfo: [NSLocalizedDescriptionKey: "Tried login but ok not received"]))
