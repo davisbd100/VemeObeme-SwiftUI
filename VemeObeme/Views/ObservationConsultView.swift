e059ffdc34cbc1e1d70fc8d51a11c5910a2af9d7
David Bárcenas Durán <davisbd100@gmail.com>
2021-04-14 10:10:18 -0500

    Fix of center bug

--- VemeObeme/Views/ObservationConsultView.swift
+++ VemeObeme/Views/ObservationConsultView.swift
@@ -16,9 +16,13 @@ struct ObservationConsultView: View {
                 .font(.custom("Avenir Book", size: 14))
                 .foregroundColor(.gray)
             Divider()
-            ForEach(observationList, id: \.self){ value in
-                ReviewObservationButton(title: value.codigo ?? "Observación", incidentDate: formatStringDateForShowingShort(date: value.fechaObservacion ?? "2020-05-03T09:19:15.000+0000"), observationDate: formatStringDateForShowingShort(date: value.fechaRegistro ?? "2020-05-03T09:19:15.000+0000"), destinationView: AnyView(ObservationConsultInfoView(observation: value)))
-            }
+            VStack{
+                ForEach(observationList, id: \.self){ value in
+                    ReviewObservationButton(title: value.codigo ?? "Observación", incidentDate: formatStringDateForShowingShort(date: value.fechaObservacion ?? "2020-05-03T09:19:15.000+0000"), observationDate: formatStringDateForShowingShort(date: value.fechaRegistro ?? "2020-05-03T09:19:15.000+0000"), destinationView: AnyView(ObservationConsultInfoView(observation: value)))
+                        .padding(.leading)
+                        .padding(.trailing)
+                }
+            }.padding()
             Spacer()
         }
         .onAppear(perform: {
