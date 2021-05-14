cd8b8b3536c3c16578ec1fb68c7651b5d9c46074
David Bárcenas Durán <davisbd100@gmail.com>
2021-05-13 22:57:16 -0500

    Back button change

--- VemeObeme/VemeObemeApp.swift
+++ VemeObeme/VemeObemeApp.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 @main
 struct VemeObemeApp: App {
-    init() {
-    }
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+    }
+}
+extension UINavigationController {
+    // Remove back button text
+    open override func viewWillLayoutSubviews() {
+        navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
 }
