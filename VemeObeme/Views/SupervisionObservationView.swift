6913db4728c25ab9184959bcd45ccad60c1cb515
David Bárcenas Durán <davisbd100@gmail.com>
2021-05-14 00:31:29 -0500

    Button supervision

--- VemeObeme/Views/SupervisionObservationView.swift
+++ VemeObeme/Views/SupervisionObservationView.swift
@@ -35,14 +35,16 @@ struct SupervisionObservationView: View {
                     .padding()
             }
             Group{
-                HStack{
-                    Image(systemName: "info.circle")
-                    Text("Fecha del incidente")
-                        .font(.custom("Avenir Book", size: 16))
-                        .foregroundColor(.green)
-                        .padding()
+                VStack(alignment: .leading){
+                    HStack{
+                        Image(systemName: "info.circle")
+                        Text("Fecha del incidente")
+                            .font(.custom("Avenir Book", size: 16))
+                            .foregroundColor(.green)
+                            .padding()
+                    }
+                    CustomDatePicker(value: $viewModel.observationDate, title: "Fecha del incidente", maxDate: Date(), minDate: Calendar.current.date(byAdding: .month, value: -12, to: Date())!)
                 }
-                CustomDatePicker(value: $viewModel.observationDate, title: "Fecha del incidente", maxDate: Date(), minDate: Calendar.current.date(byAdding: .month, value: -12, to: Date())!)
                 HStack{
                     Image(systemName: "info.circle")
                     Text("Hora aproximada del incidente")
@@ -56,43 +58,45 @@ struct SupervisionObservationView: View {
                             .frame(idealWidth: 344, maxWidth: 370, idealHeight: 53, maxHeight: 60)
                             .addBorder(Color.black, width: 2, cornerRadius: 20)
                 }
-                HStack{
-                    Image(systemName: "info.circle")
-                    Text("Servicio")
-                        .font(.custom("Avenir Book", size: 16))
-                        .foregroundColor(.green)
-                        .padding()
-                }
-                Picker(selection: $viewModel.serviceArea , label: Text("Area de servicio"), content: {
-                    ForEach(serviceAreas, id: \.self) { value in
-                        Text("\(value.nombre!)")
-                            .font(.custom("Avenir Book", size: 15))
+                VStack(alignment: .leading){
+                    HStack{
+                        Image(systemName: "info.circle")
+                        Text("Servicio")
+                            .font(.custom("Avenir Book", size: 16))
+                            .foregroundColor(.green)
+                            .padding()
                     }
-                })
-                .font(.custom("Avenir Book", size: 15)).padding()
-                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .background(Color.white)
-                .addBorder(Color.black, width: 2, cornerRadius: 20)
-                .onAppear(perform: {
-                    if (!isLoading && !dataIsLoaded){
-                        isLoading.toggle()
-                        loadingTitle = "Cargando areas de servicio"
-                        let dispatch = DispatchGroup()
-                        
-                        dispatch.enter()
-                        viewModel.getServiceArea{ serviceArea in
-                            self.dataIsLoaded = true
-                            self.serviceAreas = serviceArea
+                    Picker(selection: $viewModel.serviceArea , label: Text("Area de servicio"), content: {
+                        ForEach(serviceAreas, id: \.self) { value in
+                            Text("\(value.nombre!)")
+                                .font(.custom("Avenir Book", size: 15))
+                        }
+                    })
+                    .font(.custom("Avenir Book", size: 15)).padding()
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: 344, maxWidth: 370, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.white)
+                    .addBorder(Color.black, width: 2, cornerRadius: 20)
+                    .onAppear(perform: {
+                        if (!isLoading && !dataIsLoaded){
                             isLoading.toggle()
-                            loadingTitle = "Cargando"
-                            dispatch.leave()
-                        }
+                            loadingTitle = "Cargando areas de servicio"
+                            let dispatch = DispatchGroup()
+                            
+                            dispatch.enter()
+                            viewModel.getServiceArea{ serviceArea in
+                                self.dataIsLoaded = true
+                                self.serviceAreas = serviceArea
+                                isLoading.toggle()
+                                loadingTitle = "Cargando"
+                                dispatch.leave()
+                            }
 
-                        dispatch.notify(queue: .main){
-                            isFatalError = viewModel.isError
+                            dispatch.notify(queue: .main){
+                                isFatalError = viewModel.isError
+                            }
                         }
-                    }
-                })
+                    })
+                }
             }
         }
     }
